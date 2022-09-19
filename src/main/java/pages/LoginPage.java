6f8f491325ed6e14641f34e3d7d6044e1e76aceb
Cristinel Ungureanu (Engineering) <cristinel.ungureanu@softelligence.net>
2022-09-19 09:25:03 +0300

    curs9 logintest

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
 
+	public WebDriver driver;
+	
+	public void LoginPage(WebDriver driver) {
+		this.driver = driver;
+		
+	}
+	
 }
